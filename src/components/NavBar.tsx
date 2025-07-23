db052212ffd33c5fe5d8469bababef4fee7586b8
danielgbit <danielbitmobb@gmail.com>
2025-07-23 11:14:19 -0500

    Added styles to nav and Hero in process

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,21 +16,26 @@ const NavBar = () => {
       name: "Eventos",
       href: "#events",
     },
-        {
+    {
       name: "Servicios",
       href: "#services",
-    }
+    },
   ];
 
   return (
-    <header className="bg-navbar shadow-md">
+    <header className="bg-navbar shadow-md font-medium">
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-yellow-700">
-          Panadería Delicias
-        </h1>
-        <nav className="space-x-6 hidden md:flex">
+        <div className="text-2xl font-bold ">
+          <h1 className="text-xl">Restaurant And Cafe Pro</h1>
+          <p className="text-sm">A Complete Theme For Food Service</p>
+        </div>
+        <nav className="w-[70%] text-sm tracking-[0.1px] flex justify-between items-center hidden md:flex">
           {navItems.map((item, index) => (
-            <a key={index} href={item.href} className="text-gray-700 hover:text-yellow-700">
+            <a
+              key={index}
+              href={item.href}
+              className="text-gray-700 hover:text-yellow-700"
+            >
               {item.name}
             </a>
           ))}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/Button";
 
 const Hero = () => {
   return (
-    <section id="inicio" className="bg-yellow-50 py-16">
+    <section id="inicio" className="py-16 h-130">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center">
         <div className="md:w-1/2 mb-10 md:mb-0">
           <h2 className="text-4xl font-bold mb-4 text-yellow-800">
